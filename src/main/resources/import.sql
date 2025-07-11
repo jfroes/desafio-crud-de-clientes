957a29fbfc214bc563f34ab3fd75f5291b4d6f89
José Froes <josepaulo.froes@gmail.com>
2025-07-11 17:43:53 -0300

    :wrench: chore: Database config and seeding

---
+++ src/main/resources/import.sql
@@ -0,0 +1,21 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Juliane Pereira',     '176.457.010-30', 1525.00, '2007-02-15',  1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Carlos Eduardo Lima', '092.334.998-45', 3200.00, '1985-08-21', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Mariana Silva',       '123.456.789-10', 4500.00, '1990-03-10', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Paulo Henrique Costa','789.654.321-00', 1800.00, '1995-07-05', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Fernanda Rocha',      '456.789.123-88', 2500.00, '1988-11-19', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lucas Mendes',        '333.222.111-77', 3700.00, '1992-06-25', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Aline Souza',         '999.888.777-66', 2100.00, '1999-12-30', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Rodrigo Matos',       '444.555.666-22', 5000.00, '1978-04-12', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Beatriz Gonçalves',   '111.222.333-99', 2900.00, '2000-09-09', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Tiago Ferreira',      '555.666.777-11', 6100.00, '1983-01-22', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Renata Almeida',      '321.654.987-00', 2800.00, '1991-10-10', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Felipe Torres',       '654.321.789-20', 3300.00, '1987-05-23', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Camila Martins',      '789.123.456-70', 4100.00, '1993-07-14', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Diego Oliveira',      '222.333.444-55', 1700.00, '2001-03-30', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Patrícia Ribeiro',    '888.777.666-33', 2450.00, '1996-11-05', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Gabriel Nascimento',  '666.555.444-99', 6000.00, '1980-02-18', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Larissa Castro',      '111.333.555-77', 1950.00, '1998-09-12', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ricardo Barros',      '444.111.222-88', 5100.00, '1984-12-01', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Natália Duarte',      '777.888.999-11', 3600.00, '1990-04-08', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('André Vasconcelos',   '999.000.111-44', 2300.00, '1994-06-27', 2);
+
