2bf57bc2b26f9b452070713250d91627d1b39079
José Froes <josepaulo.froes@gmail.com>
2025-07-11 16:58:05 -0300

    First commit

---
+++ src/main/java/com/josefroes/desafio_crud_de_clientes/DesafioCrudDeClientesApplication.java
@@ -0,0 +1,13 @@
+package com.josefroes.desafio_crud_de_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCrudDeClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCrudDeClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josefroes/desafio_crud_de_clientes/DesafioCrudDeClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josefroes.desafio_crud_de_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCrudDeClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
