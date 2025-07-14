60e8aef4df6ed5f05afaf9259339025044630670
José Froes <josepaulo.froes@gmail.com>
2025-07-13 22:43:54 -0300

    :sparkles: feat: App layers and findAll w/ Page

--- src/main/java/com/josefroes/desafio_crud_de_clientes/controllers/ClientController.java
+++ src/main/java/com/josefroes/desafio_crud_de_clientes/controllers/ClientController.java
@@ -7,10 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -30,4 +27,7 @@ public class ClientController {
         Page<ClientDTO> dto = service.findAll(pageable);
         return  ResponseEntity.ok(dto);
     }
+
+    //TODO: Update by id
+    //TODO: Delete by id
 }
